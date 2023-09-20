98275139f6b3f0a1f88d85240e8fa73192e9c5a5
Isabel Collado Herrera <icolladoherrera@gmail.com>
2023-09-20 13:01:04 +0200

    "add link to landing page in the image"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import titleHarry from '../images/titleHarry.png.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="div_img_header">
-        <img src={titleHarry} alt="title Harry" className="title_header" />
+        <Link to="/">
+          <img src={titleHarry} alt="title Harry" className="title_header" />
+        </Link>
       </div>
     </header>
   );
